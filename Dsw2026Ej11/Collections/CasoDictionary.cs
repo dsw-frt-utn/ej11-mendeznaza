0a61cfaa78f59d3e01c956061fed36f5dd2322e4
Nazarena Mendez <nazarena.mendez@alu.frt.utn.edu.ar>
2026-07-30 17:27:51 -0300

    Correccion: archivos sin subir

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,40 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+
+    Dictionary<int, Alumno> alumnos = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumnos(Alumno alumno)
+    {
+        alumnos.Add(alumno.Id, alumno);
+    }
+
+    public Alumno BuscarAlumno(int legajo)
+    {
+
+        if (alumnos.ContainsKey(legajo))
+        {
+            return alumnos[legajo];
+        }
+        else
+        {
+            Console.WriteLine("No existe");
+        }
+
+        return null;
+    }
+
+    public Dictionary<int, Alumno> RetornarDiccionario()
+    {
+        return alumnos;
+    }
+
+    public void RemoverAlumno(int legajo)
+    {
+        if (alumnos.ContainsKey(legajo))
+        {
+            alumnos.Remove(legajo);
+        }
+    }
 }
+
